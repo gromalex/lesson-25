ea3d117b4615ef94e7dc4e44698698b94f337a48
Alexey Gulo <alexeygulo@gmail.com>
2021-07-17 20:44:35 +0300

    Upd lesson

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,5 +1,53 @@
-import { Editor } from './editor';
+// import { Editor } from './editor';
 
-new Editor()
+// new Editor()
 
-console.log('Test')
+// console.log('Test')
+
+// ----------------------------------------------------------------
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const rootElement = document.querySelector('#root')
+
+// Class component
+class Card extends React.Component {
+  render () {
+    return (
+      <div className="card">
+        <div className="card__title">{this.props.title}</div>
+        <div className="card__description">{this.props.description}</div>
+      </div>
+    )
+  }
+}
+
+// Function component
+function Button (props) {
+  let classList = 'btn'
+
+  if (props.type) {
+    classList += ' btn-' + props.type
+  }
+
+  const handleClick = () => console.log('Click!')
+
+  return <button className={classList} onClick={handleClick}>{props.children}</button>
+}
+
+// ReactDOM.render(
+//   <Card title="Hello" description="Мы будем называть такие компоненты «функциональными», так как они буквально являются функциями." />,
+//   rootElement
+// )
+
+function App (props) {
+  return <div>{props.children}</div>
+}
+
+ReactDOM.render(
+  <App>
+    <Button type="primary">Кнопка</Button>
+
+  </App>,
+  rootElement
+)
